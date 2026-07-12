9997c910b35e31cbbb651977596a187313c00379
shawki <shawki2207112@stud.kuet.ac.bd>
2026-07-12 19:45:35 +0600

    Update login page

--- resources/views/home/login.blade.php
+++ resources/views/home/login.blade.php
@@ -46,7 +46,7 @@
                     <label for="email">Email Address</label>
                     <input type="email" id="email" name="email" placeholder="Enter your email" required>
                 </div>
-
+                
                 <div class="form-group">
                     <label for="password">Password</label>
                     <input type="password" id="password" name="password" placeholder="Enter your password" required>
